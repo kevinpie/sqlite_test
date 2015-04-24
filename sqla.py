a39a99bbcc7826699e0586cfd520b21bc4bbca95
Kevin Koshy <kevin@logicmash.com>
2015-04-24 23:54:18 +0300

    Testing ExecuteMany with 'where'

--- sqla.py
+++ sqla.py
@@ -5,9 +5,24 @@ with sqlite3.connect("new.db") as connection:
 
     cities = [
         ('Boston', 'MA', 600000),
-        ('Chicago', 'IL', 2700000),
+        ('Los Angeles', 'CA', 38000000),
         ('Houston', 'TX', 2100000),
-        ('Phoenix', 'AZ', 1500000)
+        ('Philadelphia', 'PA', 1500000),
+        ('San Antonio', 'TX', 1400000),
+        ('San Diego', 'CA', 130000),
+        ('Dallas', 'TX', 1200000),
+        ('San Jose', 'CA', 900000),
+        ('Jacksonville', 'FL', 800000),
+        ('Indianapolis', 'IN', 800000),
+        ('Austin', 'TX', 800000),
+        ('Detroit', 'MI', 700000)
     ]
 
-    c.executemany('INSERT INTO POPULATION VALUES(?, ?, ?)', cities)
+    c.executemany("INSERT INTO Population VALUES(?, ?, ?)", cities)
+
+    c.execute("SELECT * FROM Population WHERE Population > 1000000")
+
+    rows = c.fetchall()
+
+    for r in rows:
+        print r[0], r[1], r[2]
